d4c252fb790a03baaf239f653c14ca5dc933e2fd
Rakesh <sharma742762@gmail.com>
2023-05-18 11:33:08 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,6 +13,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "1cfd9c12-2c35-4462-a5f4-75403af97042"
+  client_id       = "be9fa68e-2f87-4719-9861-4c13ebfb6b93"
+  client_secret   = var.client_secret
+  tenant_id       = "4c6f1364-8db1-4e57-95ef-b3cf7dd7d4c9"
 }
 
 #Generate randon name for all resources
